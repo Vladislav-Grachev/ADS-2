62c0e2a9434877f00296e06f26d8bc016a930e74
Vladislav Grachev <99138298+Vladislav-Grachev@users.noreply.github.com>
2022-03-06 20:16:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,16 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
-
 double pown(double value, uint16_t n) {
-  double resl = 1;
+  double res = 1;
   if (n == 0) {
-    return resl;
-}
-  else {
+    return res;
+} else {
     for (int i = 1; i <= n; i++) {
-      resl = value * resl;
+      res = value * res;
     }
   }
-  return resl;
+  return res;
 }
 uint64_t fact(uint16_t n) {
   uint16_t factr = 1;
@@ -23,28 +20,28 @@ uint64_t fact(uint16_t n) {
     return n * fact(n - 1);
 }
 double calcItem(double x, uint16_t n) {
-  double resl;
-  resl = pown(x, n) / fact(n);
-  return resl;
+  double res;
+  res = pown(x, n) / fact(n);
+  return res;
 }
 double expn(double x, uint16_t count) {
-  double resl = 0;
+  double res = 0;
   for (int i = 0; i <= count; i++) {
-    resl += calcItem(x, i);
+    res += calcItem(x, i);
   }
   return res;
 }
 double sinn(double x, uint16_t count) {
-  double resl = 0;
+  double res = 0;
   for (int i = 1; i <= count; i++) {
-    resl += pown(-1.0, (i - 1)) * calcItem(x, (2 * i - 1));
+    res += pown(-1.0, (i - 1)) * calcItem(x, (2 * i - 1));
   }
-  return resl;
+  return res;
 }
 double cosn(double x, uint16_t count) {
-  double resl = 0;
+  double res = 0;
   for (int i = 1; i <= count; i++) {
-    resl += pown(-1.0, (i - 1)) * calcItem(x, (2 * i - 2));
+    res += pown(-1.0, (i - 1)) * calcItem(x, (2 * i - 2));
   }
-  return resl;
+  return res;
 }
